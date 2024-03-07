d2f278c7efe71e07c75530930bf10c70d435a02f
Minasseh <mnsalaro@yahoo.com>
2024-03-07 11:58:39 +0300

    Changes

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,9 +4,9 @@ import requests
 
 def recurse(subreddit, hot_list=[], after=None):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0'}
     params = {'limit': 100, 'after': after}
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
