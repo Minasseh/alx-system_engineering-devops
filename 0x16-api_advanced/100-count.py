1b04d7044c931922a48a60c12af0a6491bb20972
Minasseh <mnsalaro@yahoo.com>
2024-03-07 12:57:16 +0300

    Count

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Recurrsive function that queries Reddit API"""
+import requests
+
+def count_words(subreddit, word_list, after=None, counts={}):
+    if after is None:
+        counts.clear()
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    params = {'limit': 100, 'after': after}
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+
+        for post in posts:
+            title = post['data']['title'].lower()
+            for word in word_list:
+                if word.lower() in title:
+                    counts[word.lower()] = counts.get(word.lower(), 0) + 1
+
+        after = data['data']['after']
+        if after:
+            return count_words(subreddit, word_list, after, counts)
+        else:
+            sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+            for word, count in sorted_counts:
+                print(f"{word}: {count}")
+    else:
+        print(None)
