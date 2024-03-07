a7b703fc1e0ca10a0678acf0da68cf31baf9df3e
Minasseh <mnsalaro@yahoo.com>
2024-03-07 10:14:30 +0300

    Subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-'''
-module for the Reddit API
-'''
-
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
-def number_of_subscribers(subscribers):
-    '''function that queries and returns the number of subscribers '''
-    headers = {'User-agent': 'test'}
-    res = requests.get("https://www.reddit.com/r/{}/about.json"
-                       .format(subscribers), headers=headers)
-    try:
-        subs = res.json()['data']['subscribers']
-    except Exception:
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    return subs
+    results = response.json().get("data")
+    return results.get("subscribers")
